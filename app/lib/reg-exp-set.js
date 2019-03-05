a7418190fbcc531cbb72adc06945307984fc060c
Andrea Cardaci <cyrus.and@gmail.com>
2019-03-05 13:49:16 +0100

    Remove already fixed TODO item

--- app/lib/reg-exp-set.js
+++ app/lib/reg-exp-set.js
@@ -57,7 +57,6 @@ class RegExpSet {
     }
 }
 
-// TODO also escape newline and such as the break the regexp? try this
 // XXX https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 function escapeRegExp(string) {
     return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
