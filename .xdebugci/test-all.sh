bf41ec90aa48411520d2d6ee8e7b23db1a8b1057
Andrea Cardaci <cyrus.and@gmail.com>
2019-03-21 20:16:35 +0100

    Squashed 'ext/' changes from 973f8646..5e00da5c
    5e00da5c Merge branch 'xdebug_2_7'
    e4341afb Merged pull request #460
    42525caa Fixed issue #1647: Memory corruption when a conditional breakpoint is used
    2cf17c78 Make ingestion script less noisy
    9df802a7 Use upsert for ingestion
    e7c1506a Make sure to trim the newline off
    0470e7ce master is now 2.8.0dev
    df6e9ff6 Merge branch 'xdebug_2_7'
    33967d87 Branch Xdebug 2.7
    88ab3ea8 Add PHP and Xdebug versions to CI ingester
    7d3089b6 Back to -dev
    7967c18d Merged pull request #457
    df17570f Fixed issue #1641: Move getpid() syscall so it gets called less often when not remote debugging
    29124010 Merge branch 'ci'
    cc7c2748 Minimal CI
    136f37dd Improve release process guidelines
    bc0b7471 Go with 2.7.0
    5f90d71f Merged pull request #456
    a69af58f Fixed issue #1577: Code coverage path analysis with chained catch fails
    
    git-subtree-dir: ext
    git-subtree-split: 5e00da5c9e98711ecdf7351eb7ba95afa04a7f80

--- .xdebugci/test-all.sh
+++ .xdebugci/test-all.sh
@@ -2,10 +2,17 @@
 
 PATTERN=${1:-'7.3*'}
 
+PHP=`which php`
+if [[ "${PHP}" == "" ]]; then
+	echo "Can't find a PHP binary on the path"
+	exit
+fi
+
 PHP_DIR=${PHP_DIR:-/usr/local/php}
 SYSTEM_CORES=`nproc`
 NPROC=${NPROC:-$SYSTEM_CORES}
-
+MYFILE=`realpath $0`
+MYDIR=`dirname ${MYFILE}`
 PHPS=`ls -vd ${PHP_DIR}/${PATTERN}`
 
 mkdir -p /tmp/ptester
@@ -34,10 +41,14 @@ for i in `seq 0 $MAX`; do
 	GroupName=`printf group%03d.lst $i`
 
 	if [ -s /tmp/ptester/$GroupName ]; then
-		./test-thread.sh $i `cat /tmp/ptester/$GroupName` &
+		${MYDIR}/test-thread.sh $i `cat /tmp/ptester/$GroupName` &
 	fi
 done
 
 wait
+
+echo -n "Storing test results... "
+
+php -dextension=mongodb.so ${MYDIR}/ingest.php
 
 echo "DONE"
