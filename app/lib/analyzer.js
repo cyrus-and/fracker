55f17d327a55b24050206d30e0ffc81634994aeb
Andrea Cardaci <cyrus.and@gmail.com>
2019-03-27 16:13:22 +0100

    Move the custom object stringifier to its own class

--- app/lib/analyzer.js
+++ app/lib/analyzer.js
@@ -1,4 +1,5 @@
 const color = require('./color.js');
+const ObjectStringifier = require('./object-stringifier.js');
 const ObjectWalker = require('./object-walker.js');
 const RegExpSet = require('./reg-exp-set.js');
 const term = require('./term.js');
@@ -53,104 +54,10 @@ function run(server, options = {}) {
             term.out(color.reset(`${indentation}${callId}${marker}${functionName}${argumentList}${fileInfo}`), request.id);
         }
 
-        function stringifyObject(object) {
-            let matched = false;
-            let string = '';
-            if (typeof(object) === 'object' && object !== null) {
-                if (Array.isArray(object)) {
-                    string += '[';
-                    object.forEach((element, i) => {
-                        // separator
-                        if (i !== 0) {
-                            string += ', ';
-                        }
-
-                        // stringify the element
-                        const result = stringifyObject(element);
-                        if (!result) {
-                            return;
-                        }
-                        string += result.string;
-                        matched = matched || result.matched;
-                    });
-                    string += ']';
-                } else {
-                    string += '{';
-                    Object.keys(object).forEach((key, i) => {
-                        let result;
-
-                        // separator
-                        if (i !== 0) {
-                            string += ', ';
-                        }
-
-                        // stringify the key
-                        result = stringifyObject(key);
-                        if (!result) {
-                            return;
-                        }
-                        string += result.string;
-                        matched = matched || result.matched;
-
-                        // separator
-                        string += ': ';
-
-                        // stringify the value
-                        result = stringifyObject(object[key]);
-                        if (!result) {
-                            return;
-                        }
-                        string += result.string;
-                        matched = matched || result.matched;
-                    });
-                    string += '}';
-                }
-            } else {
-                // non-string objects are just converted, strings are matched and highlighted
-                if (typeof(object) !== 'string') {
-                    string += String(object);
-                } else {
-                    // match against exclusions
-                    if (RegExpSet.exclude(object, excludeArgumentsRegexp)) {
-                        return null;
-                    } else {
-                        const components = [];
-                        let index = 0;
-                        let match;
-
-                        // loop over the matches
-                        const regexp = argumentsRegexp.get();
-                        while (!argumentsRegexp.isEmpty() && (match = regexp.exec(object)) !== null) {
-                            // add the span before the match
-                            components.push(JSON.stringify(object.slice(index, match.index)).slice(1, -1));
-
-                            // move the index after the match
-                            index = regexp.lastIndex;
-
-                            // add the match itself
-                            components.push(color.highlight(JSON.stringify(object.slice(match.index, index)).slice(1, -1)));
-
-                            // avoid an infinite loop for zero-sized matches
-                            if (regexp.lastIndex === match.index) {
-                                regexp.lastIndex++;
-                            }
-                        }
-
-                        // add the remaining part
-                        components.push(JSON.stringify(object.slice(index, object.length)).slice(1, -1));
-
-                        // update status
-                        matched = (components.length > 1);
-                        string += `"${components.join('')}"`;
-                    }
-                }
-            }
-            return {matched, string};
-        }
-
         const isWebRequest = !!request.server.REQUEST_METHOD;
         const matchedCalls = new Set();
         const argumentsRegexp = new RegExpSet(options.arguments, options.ignoreCase); // per-request
+        const stringifier = new ObjectStringifier(argumentsRegexp, excludeArgumentsRegexp);
         const stackTrace = [];
         let lastMatchedLevel;
         let inMatchedFunction;
@@ -264,7 +171,7 @@ function run(server, options = {}) {
                 let atLeastOneMatched = false;
                 for (const argument of call.arguments) {
                     // stringify the argument
-                    const result = stringifyObject(argument.value);
+                    const result = stringifier.stringify(argument.value);
 
                     // abort the call if this argument is excluded by the regexp
                     if (!result) {
---
+++ app/lib/object-stringifier.js
@@ -0,0 +1,106 @@
+const color = require('./color.js');
+const RegExpSet = require('./reg-exp-set.js');
+
+class ObjectStringifier {
+    constructor(argumentsRegexp, excludeArgumentsRegexp) {
+        this._argumentsRegexp = argumentsRegexp;
+        this._excludeArgumentsRegexp = excludeArgumentsRegexp;
+    }
+
+    stringify(object) {
+        let matched = false;
+        let string = '';
+        if (typeof(object) === 'object' && object !== null) {
+            if (Array.isArray(object)) {
+                string += '[';
+                object.forEach((element, i) => {
+                    // separator
+                    if (i !== 0) {
+                        string += ', ';
+                    }
+
+                    // stringify the element
+                    const result = this.stringify(element);
+                    if (!result) {
+                        return;
+                    }
+                    string += result.string;
+                    matched = matched || result.matched;
+                });
+                string += ']';
+            } else {
+                string += '{';
+                Object.keys(object).forEach((key, i) => {
+                    let result;
+
+                    // separator
+                    if (i !== 0) {
+                        string += ', ';
+                    }
+
+                    // stringify the key
+                    result = this.stringify(key);
+                    if (!result) {
+                        return;
+                    }
+                    string += result.string;
+                    matched = matched || result.matched;
+
+                    // separator
+                    string += ': ';
+
+                    // stringify the value
+                    result = this.stringify(object[key]);
+                    if (!result) {
+                        return;
+                    }
+                    string += result.string;
+                    matched = matched || result.matched;
+                });
+                string += '}';
+            }
+        } else {
+            // non-string objects are just converted, strings are matched and highlighted
+            if (typeof(object) !== 'string') {
+                string += String(object);
+            } else {
+                // match against exclusions
+                if (RegExpSet.exclude(object, this._excludeArgumentsRegexp)) {
+                    return null;
+                } else {
+                    const components = [];
+                    let index = 0;
+                    let match;
+
+                    // loop over the matches
+                    const regexp = this._argumentsRegexp.get();
+                    while (!this._argumentsRegexp.isEmpty() && (match = regexp.exec(object)) !== null) {
+                        // add the span before the match
+                        components.push(JSON.stringify(object.slice(index, match.index)).slice(1, -1));
+
+                        // move the index after the match
+                        index = regexp.lastIndex;
+
+                        // add the match itself
+                        components.push(color.highlight(JSON.stringify(object.slice(match.index, index)).slice(1, -1)));
+
+                        // avoid an infinite loop for zero-sized matches
+                        if (regexp.lastIndex === match.index) {
+                            regexp.lastIndex++;
+                        }
+                    }
+
+                    // add the remaining part
+                    components.push(JSON.stringify(object.slice(index, object.length)).slice(1, -1));
+
+                    // update status
+                    matched = (components.length > 1);
+                    string += `"${components.join('')}"`;
+                }
+            }
+        }
+        return {matched, string};
+    }
+}
+
+module.exports = ObjectStringifier;
