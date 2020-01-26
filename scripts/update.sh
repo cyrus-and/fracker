4f4ba24da6cb3128fb91a1e4fa2ae248a57523fc
Andrea Cardaci <cyrus.and@gmail.com>
2020-01-26 21:06:11 +0100

    Allow to update Xdebug from a specific reference

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,7 @@
 
 set -e
 
-git subtree pull -P ext --squash -m 'Update Xdebug' https://github.com/xdebug/xdebug master
+ref="${1:?Specify a remote reference}"
+git subtree pull -P ext --squash -m "Update Xdebug to $ref" https://github.com/xdebug/xdebug "$ref"
 git gc --aggressive
 git prune
