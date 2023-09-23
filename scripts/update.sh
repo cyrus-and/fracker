e57c8ef837e8d8dcbe8bdc80ef2a80f1f64b8938
Andrea Cardaci <cyrus.and@gmail.com>
2023-09-23 12:23:08 +0200

    Drop the now obsolete update script

--- scripts/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-ref="${1:?Specify a remote reference}"
-git subtree pull -P ext --squash -m "Update Xdebug to $ref" https://github.com/xdebug/xdebug "$ref"
-git gc --aggressive
-git prune
